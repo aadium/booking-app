ec8c66af93ae6edbb9ea8083e713b77ff022aa84
Aadi Umrani <aadi.p.um@gmail.com>
2023-09-10 16:35:43 -0400

    Modified the profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,7 @@
+import 'package:booking_app/widgets/textbuttons/accept_text_button.dart';
+import 'package:booking_app/widgets/textbuttons/reject_text_button.dart';
+import 'package:booking_app/widgets/buttons/secondary_button.dart';
+import 'package:booking_app/widgets/textboxes/text_box.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +22,37 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  void AddUser() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add another user'),
+          content: SizedBox(
+            height: 180,
+            child: Column(
+              children: [
+                CustomTextField(labelText: 'Enter name'),
+                CustomTextField(labelText: 'Enter phone number'),
+                CustomTextField(labelText: 'Enter email ID'),
+              ],
+            ),
+          ),
+          actions: [
+            AcceptTextButton(
+              text: 'Add user',
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            RejectTextButton(
+              text: 'Cancel',
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,27 +88,77 @@ class _ProfilePageState extends State<ProfilePage> {
               ),
             ),
             const SizedBox(height: 15),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal, // Enable horizontal scrolling
-                child: Table(
-                  defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                  columnWidths: {
-                    0: IntrinsicColumnWidth(), // Adjusts width based on content
-                    1: IntrinsicColumnWidth(),
-                    2: IntrinsicColumnWidth(),
-                  },
-                  children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+              child: Table(
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                columnWidths: {
+                  0: IntrinsicColumnWidth(), // Adjusts width based on content
+                  1: IntrinsicColumnWidth(),
+                  2: IntrinsicColumnWidth(),
+                },
+                children: [
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.all(8.0), // Add cell padding
+                          child: Center(
+                            child: Text(
+                              'Name',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      TableCell(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.all(8.0), // Add cell padding
+                          child: Center(
+                            child: Text(
+                              'Phone Number',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      TableCell(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.all(8.0), // Add cell padding
+                          child: Center(
+                            child: Text(
+                              'Email',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  for (final userData in widget.userDataList)
                     TableRow(
                       children: [
                         TableCell(
                           child: Padding(
-                            padding: const EdgeInsets.all(8.0), // Add cell padding
+                            padding:
+                                const EdgeInsets.all(8.0), // Add cell padding
                             child: Center(
                               child: Text(
-                                'Name',
+                                userData['name'],
                                 style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
                                 ),
                               ),
                             ),
@@ -81,13 +166,13 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         TableCell(
                           child: Padding(
-                            padding: const EdgeInsets.all(8.0), // Add cell padding
+                            padding:
+                                const EdgeInsets.all(8.0), // Add cell padding
                             child: Center(
                               child: Text(
-                                'Phone Number',
+                                userData['phoneNum'].toString(),
                                 style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
                                 ),
                               ),
                             ),
@@ -95,13 +180,13 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         TableCell(
                           child: Padding(
-                            padding: const EdgeInsets.all(8.0), // Add cell padding
+                            padding:
+                                const EdgeInsets.all(8.0), // Add cell padding
                             child: Center(
                               child: Text(
-                                'Email',
+                                userData['email'],
                                 style: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
                                 ),
                               ),
                             ),
@@ -109,53 +194,11 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                       ],
                     ),
-                    for (final userData in widget.userDataList)
-                      TableRow(
-                        children: [
-                          TableCell(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0), // Add cell padding
-                              child: Center(
-                                child: Text(
-                                  userData['name'],
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          TableCell(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0), // Add cell padding
-                              child: Center(
-                                child: Text(
-                                  userData['phoneNum'].toString(),
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          TableCell(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0), // Add cell padding
-                              child: Center(
-                                child: Text(
-                                  userData['email'],
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                  ],
-                ),
+                ],
               ),
+            ),
+            const SizedBox(height: 20),
+            SecondaryButton(text: 'Add User', onPressed: () => AddUser())
           ],
         ),
       ),
