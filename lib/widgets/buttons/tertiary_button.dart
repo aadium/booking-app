077428ed53a7f9ffc730160cb416d02d7cc102f7
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2024-01-07 17:32:30 +0300

    implemented the view booking button correction

--- lib/widgets/buttons/tertiary_button.dart
+++ lib/widgets/buttons/tertiary_button.dart
@@ -4,23 +4,25 @@ class TertiaryButton extends StatelessWidget {
   final VoidCallback onPressed;
   final child;
 
-  const TertiaryButton(
-      {required this.onPressed, required this.child});
+  const TertiaryButton({required this.onPressed, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        backgroundColor: Color.fromRGBO(219, 226, 230, 1),
-        textStyle: const TextStyle(fontSize: 17, color: Colors.black),
-        padding: const EdgeInsets.all(17.0),
-        elevation: 0
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(10),
+        color: Color.fromRGBO(219, 226, 230, 1),
       ),
-      onPressed: onPressed,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [child],
+      child: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(left: 10, right: 10, top: 10), child: child),
+          TextButton(
+              onPressed: onPressed,
+              child: Text('Click for more details',
+                  style: TextStyle(
+                      fontSize: 20, color: Color.fromRGBO(42, 54, 59, 1))))
+        ],
       ),
     );
   }
