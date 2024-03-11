39265be4c90f84b00eb99489b8e1b7d3a64f2886
Aadi Umrani <aadi.p.um@gmail.com>
2024-03-11 17:58:55 +0300

    Update loader_2.dart

--- lib/widgets/loaders/loader_2.dart
+++ lib/widgets/loaders/loader_2.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class Loader2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      color: Color.fromRGBO(42, 54, 59, 1),
+    return SizedBox(
+      height: 150,
+      width: 150,
+      child: CircularProgressIndicator(
+        color: Color.fromRGBO(42, 54, 59, 1),
+        strokeWidth: 10,
+        strokeCap: StrokeCap.round,
+      ),
     );
   }
 }
