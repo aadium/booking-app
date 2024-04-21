22044c8d9b3da7f3f9ac850148f411f3e02374e0
Aadi Umrani <aadi@playtheory.xyz>
2024-04-21 17:24:13 +0300

    Modified code to always keep the end times greater than the start times

--- lib/pages/clubhouse/book_clubhouse.dart
+++ lib/pages/clubhouse/book_clubhouse.dart
@@ -188,49 +188,6 @@ class _BookClubHouse extends State<BookClubHouse> {
                     labelText: 'Additional requests (optional)',
                     maxLines: 3,
                   )),
-              const SizedBox(height: 20),
-              Table(children: [
-                TableRow(children: [
-                  FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: DropdownButtonFormField<dynamic>(
-                      // Start Time
-                      value: startTimeList[0],
-                      onChanged: (value) {
-                        setState(() {
-                          selectedStartingTime =
-                              bookingMinorFunctions.parseTimeOfDay(value!);
-                        });
-                      },
-                      items: startTimeList.map((time) {
-                        return DropdownMenuItem<dynamic>(
-                          value: time,
-                          child: Text(time),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                  FractionallySizedBox(
-                    widthFactor: 0.8,
-                    child: DropdownButtonFormField<dynamic>(
-                      // End Time
-                      value: endTimeList[0],
-                      onChanged: (value) {
-                        setState(() {
-                          selectedEndingTime =
-                              bookingMinorFunctions.parseTimeOfDay(value!);
-                        });
-                      },
-                      items: endTimeList.map((time) {
-                        return DropdownMenuItem<dynamic>(
-                          value: time,
-                          child: Text(time),
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ])
-              ]),
               const SizedBox(height: 30),
               FractionallySizedBox(
                 widthFactor: 0.9,
@@ -246,11 +203,63 @@ class _BookClubHouse extends State<BookClubHouse> {
                         setState(() {
                           isDateSelectionDone = true;
                           selectedDate =
-                              asyncDate == null ? selectedDate : asyncDate;
+                          asyncDate == null ? selectedDate : asyncDate;
                         });
                       }),
                 ),
               ),
+              isDateSelectionDone ? Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Table(children: [
+                    TableRow(children: [
+                      FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: DropdownButtonFormField<dynamic>(
+                          // Start Time
+                          value: startTimeList[0],
+                          onChanged: (value) {
+                            setState(() {
+                              selectedStartingTime = bookingMinorFunctions.parseTimeOfDay(value!);
+                              int selectedIndex = timeList.indexOf(value);
+                              if (selectedIndex >= 0 && selectedIndex < timeList.length - 1) {
+                                endTimeList = [firstElementOfEndTimeList, ...timeList.sublist(selectedIndex + 1)];
+                              } else {
+                                endTimeList = [firstElementOfEndTimeList];
+                              }
+                            });
+                          },
+                          items: startTimeList.map((time) {
+                            return DropdownMenuItem<dynamic>(
+                              value: time,
+                              child: Text(time),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: DropdownButtonFormField<dynamic>(
+                          // End Time
+                          value: endTimeList[0],
+                          onChanged: (value) {
+                            setState(() {
+                              selectedEndingTime =
+                                  bookingMinorFunctions.parseTimeOfDay(value!);
+                            });
+                          },
+                          items: endTimeList.map((time) {
+                            return DropdownMenuItem<dynamic>(
+                              value: time,
+                              child: Text(time),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ])
+                  ]),
+                ],
+              ) : const SizedBox(height: 0),
               const SizedBox(height: 30),
               FractionallySizedBox(
                 widthFactor: 0.9,
